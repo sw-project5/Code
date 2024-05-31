81843b75cb238bef08bfec1042bd35f8ca8c1714
Kwon <160165670+Kwondongkyun@users.noreply.github.com>
2024-05-31 16:16:16 +0900

    소공 프로젝트 5조

--- project/manager_window.py
+++ project/manager_window.py
@@ -1,32 +1,39 @@
-import customtkinter as ctk
+import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
 import wordDB  # wordDB.py 파일을 가져옵니다.
 import json
 import os
-import tkinter as tk
-# 색상 설정
-bgColor = "#FFDFB9"
-fgColor = "#A4193D"
-hoverColor = "#C850C0"
+import customtkinter 
+from customtkinter import *
+from user_window import UserWindow
+
 
 class ManagerWindow:
     def __init__(self):
-        self.window = ctk.CTk()
+        self.bgColor="#FFDFB9"
+        self.fgColor="#A4193D"
+        self.hoverColor="#C850C0"
+        self.window = customtkinter.CTkToplevel()
         self.window.title("Word Manager")
-        self.window.geometry("450x500+100+100")
+        self.window.geometry("600x700+100")
         self.window.resizable(False, False)
-        self.window.config(background=bgColor)
+        self.window.config(background=self.bgColor)
 
         self.words = wordDB.words
+
+        self.window.attributes("-topmost", True)
+        self.window.update()  # Update the window to apply the topmost attribute
+        self.window.attributes("-topmost", False)
 
         self.create_widgets()
 
     def update_word_listbox(self):
         try:
-            self.word_listbox.delete(0, ctk.END)
+            self.word_listbox.delete(0, tk.END)
             for word in self.words:
                 for eng, kor in word.items():
-                    self.word_listbox.insert(ctk.END, f"{eng}: {kor}")
+                    self.word_listbox.insert(tk.END, f"{eng}: {kor}")
         except Exception as e:
             messagebox.showerror("에러", "일시적인 오류가 발생했습니다. 나중에 다시 시도해주세요.")
 
@@ -40,8 +47,8 @@ class ManagerWindow:
                 else:
                     self.words.append({eng: kor})
                     self.update_word_listbox()
-                    self.word_entry.delete(0, ctk.END)
-                    self.meaning_entry.delete(0, ctk.END)
+                    self.word_entry.delete(0, tk.END)
+                    self.meaning_entry.delete(0, tk.END)
             else:
                 messagebox.showwarning("Input Error", "단어와 뜻 모두 입력해주세요.")
         except Exception as e:
@@ -57,8 +64,8 @@ class ManagerWindow:
                 if eng and kor:
                     self.words[index] = {eng: kor}
                     self.update_word_listbox()
-                    self.word_entry.delete(0, ctk.END)
-                    self.meaning_entry.delete(0, ctk.END)
+                    self.word_entry.delete(0, tk.END)
+                    self.meaning_entry.delete(0, tk.END)
                 else:
                     messagebox.showwarning("Input Error", "단어와 뜻 모두 입력해주세요.")
             else:
@@ -72,8 +79,8 @@ class ManagerWindow:
                         break
                 if found:
                     self.update_word_listbox()
-                    self.word_entry.delete(0, ctk.END)
-                    self.meaning_entry.delete(0, ctk.END)
+                    self.word_entry.delete(0, tk.END)
+                    self.meaning_entry.delete(0, tk.END)
                 else:
                     messagebox.showwarning("Word Not Found", "해당 단어는 존재하지 않습니다.")
         except Exception as e:
@@ -91,8 +98,8 @@ class ManagerWindow:
                 if word == eng and meaning == kor:
                     del self.words[index]
                     self.update_word_listbox()
-                    self.word_entry.delete(0, ctk.END)
-                    self.meaning_entry.delete(0, ctk.END)
+                    self.word_entry.delete(0, tk.END)
+                    self.meaning_entry.delete(0, tk.END)
                 else:
                     messagebox.showwarning("Mismatch Error", "해당 단어와 뜻이 일치하지 않습니다.")
             else:
@@ -104,8 +111,8 @@ class ManagerWindow:
                         break
                 if found:
                     self.update_word_listbox()
-                    self.word_entry.delete(0, ctk.END)
-                    self.meaning_entry.delete(0, ctk.END)
+                    self.word_entry.delete(0, tk.END)
+                    self.meaning_entry.delete(0, tk.END)
                 else:
                     messagebox.showwarning("Word Not Found", "해당 단어와 뜻이 일치하지 않습니다.")
         except Exception as e:
@@ -119,7 +126,6 @@ class ManagerWindow:
                 json.dump(words_data["words"], file, ensure_ascii=False, indent=4)
         except Exception as e:
             messagebox.showerror("에러", "일시적인 오류가 발생했습니다. 나중에 다시 시도해주세요.")
-
     def open_login_window(self):
         import login_window
         login_window.open_login_window(self.window)
@@ -132,41 +138,76 @@ class ManagerWindow:
                 self.window.destroy()
 
     def create_widgets(self):
-        self.word_listbox = tk.Listbox(self.window, height=10, width=50)
-        self.word_listbox.grid(row=0, column=0, columnspan=3)
+        
+
+        self.word_listbox = tk.Listbox(self.window,height=15, width=60)
+        self.word_listbox.pack(pady=10)
+        self.word_listbox.config(background=self.bgColor)
+
+        self.start_index =  0
+        self.end_index = len(self.words)
+
+        self.canvas = tk.Canvas(self.word_listbox)
+        self.canvas.config(background=self.bgColor)
+        self.scrollbar = ttk.Scrollbar(self.word_listbox, orient="vertical", command=self.canvas.yview)
+
+        self.word_frame = tk.Frame(self.canvas)
+        self.word_frame.config(background=self.bgColor)
+        self.word_frame.bind(
+            "<Configure>",
+            lambda e: self.canvas.configure(
+                scrollregion=self.canvas.bbox("all")
+                )
+        )    
+        
+        self.canvas.create_window((0, 0), window=self.word_frame, anchor="nw")
+        self.canvas.configure(yscrollcommand=self.scrollbar.set,background=self.bgColor)
+        self.canvas.config(background=self.bgColor,width=350,height=300)
+        
+        for i, self.word in enumerate(self.words[self.start_index:self.end_index], start=self.start_index):
+            for english_word, meaning in self.word.items():
+                self.word_label = tk.Label(self.word_frame, text=f"{english_word}: {meaning}", anchor='w', justify='left', wraplength=360,background=self.bgColor,font=('맑은 고딕',14))
+                self.word_label.pack(anchor="w", padx=10, pady=5)
+
+        self.canvas.pack(side="left", fill="both", expand=True)
+        self.scrollbar.pack(side="right", fill="y")
+
         self.update_word_listbox()
 
-        ctk.CTkLabel(self.window, text="Word:", bg_color=bgColor).grid(row=1, column=0)
-        self.word_entry = ctk.CTkEntry(self.window, width=250)
-        self.word_entry.grid(row=1, column=1, columnspan=2)
+        # 단어 추가/수정/삭제를 위한 엔트리와 버튼
+        self.word_frame=customtkinter.CTkFrame(self.window,bg_color=self.bgColor,fg_color=self.bgColor)
+        self.word_frame.pack(pady=10)
+        self.word_label=customtkinter.CTkLabel(self.word_frame, text="Word:",text_color="black",bg_color=self.bgColor)
+        self.word_label.pack(side="left",padx=14)
+        self.word_entry = customtkinter.CTkEntry(self.word_frame,bg_color=self.bgColor)
+        self.word_entry.pack(side="left",padx=14)
 
-        ctk.CTkLabel(self.window, text="Meaning:", bg_color=bgColor).grid(row=2, column=0)
-        self.meaning_entry = ctk.CTkEntry(self.window, width=250)
-        self.meaning_entry.grid(row=2, column=1, columnspan=2)
+        self.meaning_frame=customtkinter.CTkFrame(self.window,bg_color=self.bgColor,fg_color=self.bgColor)
+        self.meaning_frame.pack(pady=10)
+        self.meaning_label=customtkinter.CTkLabel(self.meaning_frame, text="Meaning:",text_color="black",bg_color=self.bgColor)
+        self.meaning_label.pack(side="left",padx=5)
+        self.meaning_entry = customtkinter.CTkEntry(self.meaning_frame,bg_color=self.bgColor)
+        self.meaning_entry.pack(side="left",padx=5)
 
-        button_frame = ctk.CTkFrame(self.window, bg_color=bgColor)
-        button_frame.grid(row=3, column=0, columnspan=3, pady=10)
+        self.button_frame = customtkinter.CTkFrame(self.window,bg_color=self.bgColor,fg_color=self.bgColor)
+        self.button_frame.pack(pady=10)
+        self.add_button = customtkinter.CTkButton(self.button_frame, text="추가",text_color="white",bg_color=self.bgColor,fg_color=self.fgColor, corner_radius=32,command=lambda: self.add_word())
+        self.add_button.pack(side="left")
+        self.update_button = customtkinter.CTkButton(self.button_frame, text="수정", text_color="white",bg_color=self.bgColor,fg_color=self.fgColor,corner_radius=32,command=lambda: self.update_word())
+        self.update_button.pack(side="left",padx=7)
+        self.delete_button = customtkinter.CTkButton(self.button_frame, text="삭제",text_color="white",bg_color=self.bgColor,fg_color=self.fgColor ,corner_radius=32,command=lambda: self.delete_word())
+        self.delete_button.pack()
+        # 완료 버튼을 추가합니다
+        self.complete_button = customtkinter.CTkButton(self.window, text="저장", text_color="white",bg_color=self.bgColor,fg_color=self.fgColor,hover_color=self.hoverColor,corner_radius=32,command=lambda: self.save_words_to_file())
+        self.complete_button.pack()
 
-        add_button = ctk.CTkButton(button_frame, text="Add", command=self.add_word, 
-                                   bg_color=bgColor, fg_color=fgColor, hover_color=hoverColor)
-        add_button.grid(row=0, column=0, padx=5)
+        self.logout_button = customtkinter.CTkButton(self.window, text="로그아웃", text_color="white",bg_color=self.bgColor, fg_color=self.fgColor, hover_color=self.hoverColor, corner_radius=32, command=self.logout)
+        self.logout_button.pack(pady=10)
 
-        update_button = ctk.CTkButton(button_frame, text="Update", command=self.update_word, 
-                                      bg_color=bgColor, fg_color=fgColor, hover_color=hoverColor)
-        update_button.grid(row=0, column=1, padx=5)
+        self.window.attributes("-topmost", True)
+        self.window.lift()
+        self.window.after(100, lambda: self.window.attributes("-topmost", False))
 
-        delete_button = ctk.CTkButton(button_frame, text="Delete", command=self.delete_word, 
-                                      bg_color=bgColor, fg_color=fgColor, hover_color=hoverColor)
-        delete_button.grid(row=0, column=2, padx=5)
 
-        complete_button = ctk.CTkButton(self.window, text="Complete", command=self.save_words_to_file, 
-                                        bg_color=bgColor, fg_color=fgColor, hover_color=hoverColor)
-        complete_button.grid(row=4, column=0, columnspan=3, pady=10)
-
-        logout_button = ctk.CTkButton(self.window, text="Logout", command=self.logout, 
-                                      bg_color=bgColor, fg_color=fgColor, hover_color=hoverColor)
-        logout_button.grid(row=5, column=0, columnspan=3, pady=(10, 0))
 
         self.window.mainloop()
-
-
